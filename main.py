55371a71c7f9a8f084afa387f1c40eb7d05951f2
geowat <ldsxxs@163.com>
2023-06-17 14:52:39 -0700

    mod files

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ def extract_audio(video_file_path, audio_output_path):
     audio_clip.write_audiofile(audio_output_path)
 
 
+file_name = "test_short"
 tts = TTS("tts_models/multilingual/multi-dataset/your_tts")
 
-extract_audio("test_short.mp4", "test_short.wav")
-video_duration = VideoFileClip("test.mov").duration
+extract_audio(f'{file_name}.mp4', f'{file_name}.wav')
+video = VideoFileClip(f'{file_name}.mp4')
+video_duration = video.duration
 
 model = whisper.load_model("base")
-result = model.transcribe("test.wav", task="translate")
+result = model.transcribe(f'{file_name}.wav', task="translate")
 result["segments"][-1]["end"] = video_duration
 with open("result.json", "w") as f:
     json.dump(result, f, indent=2)
@@ -29,14 +31,13 @@ for seg in result["segments"]:
         text=content_of_speech,
         language="en",
         emotion="Neutral",
-        speaker_wav="test.wav",
-        file_path=f'tts/test_{seg["id"]}.wav',
+        speaker_wav=f'{file_name}.wav',
+        file_path=f'tts/{file_name}_{seg["id"]}.wav',
     )
-    tts_list.append((seg["start"], seg["end"], f'tts/test_{seg["id"]}.wav'))
+    tts_list.append((seg["start"], seg["end"], f'tts/{file_name}_{seg["id"]}.wav'))
 
-video = VideoFileClip("test.mov")
 
 video_clips = [video.subclip(start, end).set_audio(AudioFileClip(tts)) for (start, end, tts) in tts_list]
 
 final_clip = concatenate_videoclips(video_clips)
-final_clip.write_videofile("test_replaced_audio.mp4", audio_codec="aac")
+final_clip.write_videofile(f"{file_name}_output.mp4", audio_codec="aac")
